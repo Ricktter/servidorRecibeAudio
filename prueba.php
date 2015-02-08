30c611448568ae71ef69628f9ae514f5a87d9906
unknown <rikrdo_18_panda@hotmail.com>
2015-02-07 20:54:15 -0600

    corregido

--- prueba.php
+++ prueba.php
@@ -1,13 +1,12 @@
 <html>
 <head>
-<title>Problema</title>
+<title>RecibiendoAudio</title>
 </head>
 <body>
 <?php
 copy($_FILES['myAudioFile']['tmp_name'],$_FILES['myAudioFile']['name']);
 echo "El audio se registro en el servidor.<br>";
-$nom=$_FILES['myAudioFile']['name'];
-echo "<img src=\"$nom\">";
+
 ?>
 </body>
 </html>
